ff0dc211139e83fc6d799ad556af56ea8b391a7c
scott <scottysora@gmail.com>
2024-06-15 13:24:43 -0400

    start populate function

--- scripts/new-todo.js
+++ scripts/new-todo.js
@@ -2,3 +2,9 @@ const dropdownUsers = document.querySelector("#dropdownUsers");
 const downdownPriority = document.querySelector("#downdownPriority");
 const deadlineInput = document.querySelector("#deadlineInput");
 const descriptionInput = document.querySelector("#descriptionInput")
+
+window.onload = () => {
+    populateUserDropdown();
+    populateCategoriesDropdown();
+}
+
