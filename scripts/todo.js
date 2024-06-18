9d802ab56747e4ea7ecffc1acd7c13ef038807fb
scott <scottysora@gmail.com>
2024-06-17 23:44:53 -0400

    style todo & new todo pages

---
+++ scripts/todo.js
@@ -0,0 +1,39 @@
+async function fetchTodo(){
+    try{
+        let response = await fetch("http://localhost:8083/api/todos");
+        let todoList = await response.json();
+        let container = document.querySelector("#todoDisplayContainer")
+
+        for(let todos of todoList) {
+            let card = document.createElement("div")
+            card.className = ("card");
+
+            let category = document.createElement("h3");
+            category.innerText = `Category: ${todos.category}`
+            card.appendChild(category);
+
+            let deadline = document.createElement("h4");
+            deadline.innerText = `Due: ${todos.deadline}`
+            card.appendChild(deadline);
+
+            let priority = document.createElement("h4");
+            priority.innerText = `Priority Level: ${todos.priority}`
+            card.appendChild(priority);
+            
+            let completed = document.createElement("h4");
+            completed.innerText = `Completed: ${todos.completed}`
+            card.appendChild(completed);
+            
+            let description = document.createElement("p");
+            description.innerText = `Description: ${todos.description}`
+            card.appendChild(description);
+
+
+            container.appendChild(card);
+        }
+    } catch (error){
+        console.error("Error:",error)
+    }
+}
+
+fetchTodo()
